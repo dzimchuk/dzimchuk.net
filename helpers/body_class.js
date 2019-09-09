30241f3a40078b70ddaab58deb9dad08e9dfd7ef
Andrei Dzimchuk <andrei@dzimchuk.net>
2019-09-09 18:19:51 +0300

    add page title body class

--- helpers/body_class.js
+++ helpers/body_class.js
@@ -1,10 +1,27 @@
+var slugify = require('slugify');
+
 module.exports = function() {
     if (this.collection.includes('home'))
         return 'home-template';
     else if (this.collection.includes('posts'))
         return 'post-template'; // plus add post tags: tag-one-slug, tag-two-slug
     else if (this.collection.includes('pages'))
-        return 'page-template'; // plus add page-title-slug
+    {
+        var result = 'page-template';
+        if (this.permalink)
+        {
+            result = addClass(result, 'page', this.permalink);
+        }
+        else if (this.title)
+        {
+            result = addClass(result, 'page', this.title);
+        }
+        return result;
+    }
     else 
         return 'tag-template'; // plus tag-slug
+}
+
+function addClass(cls, prefix, title) {
+    return cls + ' ' + slugify(prefix + ' ' + title, {lower: true});
 }
