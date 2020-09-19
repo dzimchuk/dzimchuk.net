81eb8e6d05733f47e3a66daff9dc3ee9ea9d243b
Andrei Dzimchuk <andrei@dzimchuk.net>
2020-09-19 15:49:11 +0300

    serve favicon manifests from the origin to avoid cors issues

--- updateCacheControl.sh
+++ updateCacheControl.sh
@@ -7,3 +7,8 @@ for blob in $(az storage blob list -c \$web --account-name $account --sas-token 
 	echo Updating: $blob
 	az storage blob update -c \$web -n $blob --clear-content-settings false --content-cache-control "public, max-age=31536000" --account-name $account --sas-token "$sas"
 done
+
+for blob in $(az storage blob list -c \$web --account-name $account --sas-token "$sas" --prefix favicons/v1/ --query [].name -o tsv); do
+	echo Updating: $blob
+	az storage blob update -c \$web -n $blob --clear-content-settings false --content-cache-control "public, max-age=31536000" --account-name $account --sas-token "$sas"
+done
